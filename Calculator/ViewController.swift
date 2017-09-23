474b9794582711d7dbca1133165121d2edaed9a5
Student <student@setass-mac-mini-41.local>
2017-09-23 17:58:15 -0400

    Calculator's Basic Functionality

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    var firstNumber = ""
+    var secondNumber = ""
+    var op = ""
+    var hasOp = false
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -22,11 +30,85 @@ class ViewController: UIViewController {
 
     @IBAction func controlButton(_ sender: UIButton) {
           
+        if resultLabel.text=="0" || hasOp{
         
+        resultLabel.text = ""
+        
+        }
+        
+        let text = sender.titleLabel?.text
+        
+        resultLabel.text = resultLabel.text! + text!
+
+    
+    if let inputText = text{
+    switch inputText{
+    case "+","÷","X","-":
+        if firstNumber == "" {
+            resultLabel.text = "0"
+        }
+        hasOp = true
+        op = inputText
+        resultLabel.text = firstNumber
+        
+    case "=":
+           
+       resultLabel.text = String (calculate())
+      
+    case "%":
+        let number = Double(resultLabel.text!)
+        let numberDivideby100 = number!/100
+        resultLabel.text = String(numberDivideby100)
+    default:
+        if hasOp{
+            secondNumber = secondNumber + inputText
+        }else{
+            firstNumber = firstNumber  + inputText
+        }
+       }
+
         
     }
-    
-    @IBOutlet weak var result: UIView!
 
 }
+
+    @IBAction func clear(_ sender: UIButton) {
+        
+        if sender.isTouchInside {
+            resultLabel.text = "0"
+        }
+    }
+    
+    func calculate() -> Double {
+        
+      
+        let firstNumberfinal = Double(firstNumber)!
+        let secondNumberfinal = Double(secondNumber)!
+        
+        
+        switch op {
+        case "+":
+            return firstNumberfinal + secondNumberfinal
+            
+        case "-":
+            return firstNumberfinal-secondNumberfinal
+            
+        case "X":
+            return firstNumberfinal*secondNumberfinal
+            
+        case "÷":
+            return firstNumberfinal/secondNumberfinal
+        
+        default:
+            return 0
+        }
+        
+        
+    
+    
+}
+}
+
+
+
 
